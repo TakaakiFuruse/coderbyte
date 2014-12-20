b6b93d8aa2ac0e2bb6d24fb9d27fd605e1e9bfce
TakaakiFuruse <takaakifuruse@gmail.com>
2014-12-21 08:14:07 +0900

    2_32 created method and test

--- 2_32/2_32.rb
+++ 2_32/2_32.rb
@@ -5,9 +5,28 @@ Using the Ruby language, have the function RunLength(str) take the str parameter
 Input = "aabbcde" Output = "2a2b1c1d1e"
 Input = "wwwbbbw" Output = "3w3b1w"
 
+str_to_a = str.split(" ")
+
+counter_hash = Hash.new
+
+ str_to_a.each do |n|
+  if h[:#{n}].nil?
+   counter_hash[:#{n}]=1
+  else
+    counter_hash[:#{n}]+=1  
+  end  
+
+counter_hash.map{|k,i| p "#{i}#{k}" }.join("")
 
 =end
 
 def RunLength(str)
-
 end
+
+def str_to_a(str)
+end
+
+def create_counter_hash(array)
+end
+
+
--- 2_32/2_32_spec.rb
+++ 2_32/2_32_spec.rb
@@ -9,3 +9,17 @@ describe "RunLength" do
     expect(RunLength("wwwbbbw")).to eq("3w3b1w")
   end
 end
+
+describe "str_to_a" do
+  it "creates array from str" do
+  expect(str_to_a("aaa")).to eq(["a", "a", "a"])
+  end
+end
+
+describe "create_counter_hash" do
+  it "returns hash with letter as value and count as key" do
+    hash = {a: 2, b: 2}
+    array = ["a", "a", "b", "b"]
+    expect(create_counter_hash(array)).to eq(hash)
+  end
+end
