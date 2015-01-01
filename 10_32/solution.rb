386033a83182d442bd41ef012004d7eed4b8eb26
TakaakiFuruse <takaakifuruse@gmail.com>
2015-01-01 11:05:15 +0900

    all test passed

--- 10_32/solution.rb
+++ 10_32/solution.rb
@@ -1,5 +1,4 @@
 =begin
-
 Using the Ruby language, have the function CaesarCipher(str,num) take the str parameter and perform a Caesar Cipher shift on it using the num parameter as the shifting number. A Caesar Cipher works by shifting each letter in the string N places down in the alphabet (in this case N will be num). Punctuation, spaces, and capitalization should remain intact. For example if the string is "Caesar Cipher" and num is 2 the output should be "Ecguct Ekrjgt". 
 
 Input = "Hello" & num = 4
@@ -15,7 +14,7 @@ require 'pry'
 module CharMod
   def next_char(ltr)
     next_ltr = ltr.next
-    return next_char(next_ltr)
+    next_char(next_ltr)
   end
 end
 
