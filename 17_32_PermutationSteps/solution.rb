685656569659e76769fc5cf5d180f04cbf508db0
TakaakiFuruse <takaakifuruse@gmail.com>
2015-02-21 17:43:45 +0900

    17_32 test created all red

--- 17_32_PermutationSteps/solution.rb
+++ 17_32_PermutationSteps/solution.rb
@@ -1,6 +1,29 @@
 =begin
 Using the Ruby language, have the function PermutationStep(num) take the num parameter being passed and return the next number greater than num using the same digits. For example: if num is 123 return 132, if it's 12453 return 12534. If a number has no greater permutations, return -1 (ie. 999). 
 
+123 -> 132
+123453 => 12534
+999 => -1
+11121 => 11211
+41352 => 41523
+
+
+[17] pry(main)> a.permutation(3).to_a
+=> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+[21] pry(main)> b = a.permutation(3).to_a.map {|n| n.join("").to_i}
+=> [123, 132, 213, 231, 312, 321]
+
+[23] pry(main)> b.sort
+=> [123, 132, 213, 231, 312, 321]
+[24] pry(main)> b.sort.map{|n| n - 123}
+=> [0, 9, 90, 108, 189, 198]
+
+[28] pry(main)> b1[1]
+
+=> 9
+[29] pry(main)> b[1]
+=> 132
 
 =end
 
@@ -8,6 +31,5 @@ require 'pry'
 
 
 def PermutationStep(num)
-
-         
+    
 end
--- 17_32_PermutationSteps/solution_spec.rb
+++ 17_32_PermutationSteps/solution_spec.rb
@@ -7,6 +7,16 @@ require_relative "solution"
 =end
 
 describe "PermutationStep" do
-  it "" do
+  it "checks permutation of num and returns next largest permutated(?) number " do
+    expect(PermutationStep(123)).to eq(132)
+    expect(PermutationStep(123453)).to eq(123534)
+    expect(PermutationStep(11121)).to eq(11211)
+    expect(PermutationStep(41352)).to eq(41523)
   end
+  
+  it "returns -1 if it can't find permutated(?) numbers " do
+   expect(PermutationStep(999)).to eq(-1)
+  end
+
+
 end
